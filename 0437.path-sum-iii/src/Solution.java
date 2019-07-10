96b7ae12f5e97f1fdaf93da6448ecd52e0be3564
hmq201211 <879602728@qq.com>
2019-07-10 18:04:55 +0800

    20190710

---
+++ 0437.path-sum-iii/src/Solution.java
@@ -0,0 +1,33 @@
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        int self = worker(root, sum);
+        int left = pathSum(root.left, sum);
+        int right = pathSum(root.right, sum);
+        return self + left + right;
+
+    }
+
+    private int worker(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        int left = worker(root.left, sum - root.val);
+        int right = worker(root.right, sum - root.val);
+        return left + right + (root.val == sum ? 1 : 0);
+    }
+}
