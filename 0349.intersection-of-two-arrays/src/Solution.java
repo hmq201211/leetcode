27b8de219fb7f81f30db021ce3a0ecaf70f91459
hmq201211 <879602728@qq.com>
2019-07-10 16:49:58 +0800

    20190710

--- 0349.intersection-of-two-arrays/src/Solution.java
+++ 0349.intersection-of-two-arrays/src/Solution.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int[] toReturn = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        for (int num : nums1) {
+            hashMap.put(num, num);
+        }
+        for (int num : nums2) {
+            if (hashMap.containsKey(num)) {
+                toReturn[index++] = num;
+                hashMap.remove(num);
+            }
+        }
+        return Arrays.copyOf(toReturn, index);
     }
 }
