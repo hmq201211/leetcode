9be52ec3cbb24e635f9a4125154adfde641159bf
hmq201211 <879602728@qq.com>
2019-07-15 09:52:37 +0800

    20190711

--- 0003.Longest Substring Without Repeating Characters/src/Solution.java
+++ 0003.Longest Substring Without Repeating Characters/src/Solution.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        int length = s.length();
+        int answer = 0;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0, j = 0; j < length; j++) {
+            if (hashMap.containsKey(s.charAt(j))) {
+                i = Math.max(i, hashMap.get(s.charAt(j)));
+            }
+            answer = Math.max(answer, j - i + 1);
+            hashMap.put(s.charAt(j), j + 1);
+        }
+        return answer;
     }
 }
