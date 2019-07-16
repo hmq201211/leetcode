d94f8f8fd231f7ff73319f6dab8672b7c0d1db07
hmq201211 <879602728@qq.com>
2019-07-16 17:10:17 +0800

    20190716

--- 0005.longest-palindromic-substring/src/Solution.java
+++ 0005.longest-palindromic-substring/src/Solution.java
@@ -20,4 +20,84 @@ class Solution {
         }
         return toReturn;
     }
+
+    private String initString(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('#');
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(s.charAt(i));
+            stringBuilder.append('#');
+        }
+        return stringBuilder.toString();
+    }
+
+
+    public String longestPalindrome2(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return "";
+        }
+        String newString = initString(s);
+        int newLength = newString.length();
+        int[] p = new int[newLength];
+        int mx = 0;
+        int id = 0;
+        int maxI = 0;
+        int longestLength = 1;
+        boolean needCompute;
+        for (int i = 0; i < newLength; i++) {
+            if (i < mx) {
+                if (p[2 * id - i] < mx - i) {
+                    p[i] = p[2 * id - i];
+                    needCompute = false;
+                } else {
+                    p[i] = mx - i;
+                    needCompute = true;
+                }
+            } else {
+                p[i] = 1;
+                needCompute = true;
+            }
+            if (needCompute)
+                while (i - p[i] >= 0 && i + p[i] < newLength && newString.charAt(i - p[i]) == newString.charAt(i + p[i])) {
+                    p[i]++;
+                }
+
+            if (i + p[i] > mx) {
+                mx = p[i] + i;
+                id = i;
+            }
+            if (p[i] > longestLength) {
+                maxI = i;
+                longestLength = p[i];
+            }
+            System.out.println("-----------");
+            for (int j : p) {
+                System.out.print(j);
+                System.out.print(' ');
+            }
+            System.out.println("-----------");
+            for (int k = 0; k < newLength; k++) {
+                System.out.print(newString.charAt(k));
+                System.out.print(' ');
+            }
+            System.out.println("-----------");
+        }
+        return newString.substring(maxI - p[maxI] + 1, maxI + p[maxI]).replace("#", "");
+    }
+
+    public static void main(String[] args) {
+        String test = "abccbasdduadjasddddjdsjdjsjdsjd";
+        Solution solution = new Solution();
+        String toReturn = solution.longestPalindrome2(test);
+        String correct = solution.longestPalindrome(test);
+        System.out.println(correct);
+        System.out.println(toReturn);
+    }
+
+
 }
