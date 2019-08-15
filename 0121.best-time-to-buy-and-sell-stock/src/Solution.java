0475c8a6e2f77a60e05a402b27e5ae4eb5feb2e4
hmq201211 <879602728@qq.com>
2019-08-15 18:01:12 +0800

    20190815

--- 0121.best-time-to-buy-and-sell-stock/src/Solution.java
+++ 0121.best-time-to-buy-and-sell-stock/src/Solution.java
@@ -34,4 +34,20 @@ public class Solution {
         }
         return globalMaximum;
     }
+
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+
+        }
+        return dp[n - 1][0];
+    }
 }
