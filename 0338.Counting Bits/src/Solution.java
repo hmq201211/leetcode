d32e6ffc3a5eaa86db6f0d3eec62473a375b9e17
hmq201211 <879602728@qq.com>
2019-08-22 11:15:49 +0800

    190822

--- 0338.Counting Bits/src/Solution.java
+++ 0338.Counting Bits/src/Solution.java
@@ -30,4 +30,17 @@ class Solution {
         return answer;
     }
 
+    public int[] countBits_4(int num) {// 奇偶性分析 奇数:比前面的偶数多1, 偶数: 和偶数/2一样多
+        int[] answer = new int[num + 1];
+        answer[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
+                answer[i] = answer[i / 2];
+            } else {
+                answer[i] = answer[i - 1] + 1;
+            }
+        }
+        return answer;
+    }
+
 }
