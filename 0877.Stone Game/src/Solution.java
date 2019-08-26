ac9f28d014d7a23ad99294effb54b05c6b1bb857
hmq201211 <879602728@qq.com>
2019-08-26 16:13:56 +0800

    190826

---
+++ 0877.Stone Game/src/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        Pair[][] dp = new Pair[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = new Pair(0, 0);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][i].first = piles[i];
+            dp[i][i].second = 0;
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                int j = l + i - 1;
+                int left = piles[i] + dp[i + 1][j].second;
+                int right = piles[j] + dp[i][j - 1].second;
+                if (left > right) {
+                    dp[i][j].first = left;
+                    dp[i][j].second = dp[i + 1][j].first;
+                } else {
+                    dp[i][j].first = right;
+                    dp[i][j].second = dp[i][j - 1].first;
+                }
+            }
+        }
+        return dp[0][n - 1].first > dp[0][n - 1].second;
+    }
+}
+
+class Pair {
+    int first;
+    int second;
+
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
