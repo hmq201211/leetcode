83d4a413838d93480a89aead25e29b5472957f8f
hmq201211 <879602728@qq.com>
2019-12-02 23:10:53 +0800

    20191202

--- 0005.longest-palindromic-substring/src/Solution.java
+++ 0005.longest-palindromic-substring/src/Solution.java
@@ -1,100 +1,5 @@
-import com.sun.javafx.sg.prism.web.NGWebView;
-
-import java.security.PublicKey;
 
 class Solution {
-
-    public String longestPalindrome2(String s) {
-        int length = s.length();
-        if (length == 0) {
-            return "";
-        }
-        String newString = createFormattedString(s);
-        int newLength = newString.length();
-        int[] p = new int[newLength];
-        int mx = 0;
-        int id = 0;
-        int maxI = 0;
-        int longestLength = 1;
-        boolean needCompute;
-        for (int i = 0; i < newLength; i++) {
-            if (i < mx) {
-                if (p[2 * id - i] < mx - i) {
-                    p[i] = p[2 * id - i];
-                    needCompute = false;
-                } else if (p[2 * id - i] > mx - i) {
-                    p[i] = mx - i;
-                    needCompute = false;
-                } else {
-                    p[i] = mx - i;
-                    needCompute = true;
-                }
-            } else {
-                p[i] = 1;
-                needCompute = true;
-            }
-            if (needCompute)
-                while (i - p[i] >= 0 && i + p[i] < newLength && newString.charAt(i - p[i]) == newString.charAt(i + p[i])) {
-                    p[i]++;
-                }
-
-            if (i + p[i] > mx) {
-                mx = p[i] + i;
-                id = i;
-                System.out.println("updating mx to " + mx + " and id to " + id);
-            }
-
-            if (p[i] > longestLength) {
-                maxI = i;
-                longestLength = p[i];
-            }
-            System.out.println("-----------");
-            for (int j : p) {
-                System.out.print(j);
-                System.out.print(' ');
-            }
-            System.out.println("-----------");
-            for (int k = 0; k < newLength; k++) {
-                System.out.print(newString.charAt(k));
-                System.out.print(' ');
-            }
-            System.out.println("-----------");
-            for (int k = 0; k < newLength; k++) {
-                System.out.print(k);
-                System.out.print(' ');
-            }
-            System.out.println("-----------");
-        }
-        return newString.substring(maxI - p[maxI] + 1, maxI + p[maxI]).replace("#", "");
-    }
-
-    private String centerSpread(String s, int length, int centerLeft, int centerRight) {
-        while (centerLeft >= 0 && centerRight < length && s.charAt(centerLeft) == s.charAt(centerRight)) {
-            centerLeft--;
-            centerRight++;
-        }
-        return s.substring(centerLeft + 1, centerRight);
-    }
-
-    public String longestPalindrome3(String s) {
-        int length = s.length();
-        if (length == 0) {
-            return "";
-        }
-        int longestPalindrome = 1;
-        String longestPalindromeString = s.substring(0, 1);
-        for (int i = 0; i < length - 1; i++) {
-            String odd = centerSpread(s, length, i, i);
-            String even = centerSpread(s, length, i, i + 1);
-            String result = odd.length() > even.length() ? odd : even;
-            if (result.length() > longestPalindrome) {
-                longestPalindrome = result.length();
-                longestPalindromeString = result;
-            }
-        }
-        return longestPalindromeString;
-    }
-
     /**
      * @Description: main
      * test method
@@ -105,7 +10,10 @@ class Solution {
     public static void main(String[] args) {
         String test = "ababa";
         Solution solution = new Solution();
-        System.out.println(solution.longestPalindrome2(test));
+        System.out.println(solution.longestPalindromeBruteForce(test));
+        System.out.println(solution.longestPalindromeDynamicProgramming(test));
+        System.out.println(solution.longestPalindromeManacher(test));
+        System.out.println(solution.longestPalindromeCenterSpread(test));
     }
 
     /**
@@ -213,20 +121,26 @@ class Solution {
      * memo[2 * id - i] which means the longest length of the palindrome which centered at the mirror of the i (based of id)
      * There are 2 outer case:
      * 1. i >= mx:
-     *      the memo is useless, then we have to compute the memo by hand ( centered at i , spread out by both left side and right side)
+     * ------the memo is useless, then we have to compute the memo by hand ( centered at i , spread out by both left side and right side)
      * 2. i < mx:
-     *      the memo can help us! there are there inner cases here:
-     *      1: memo[2 * id - i] < mx - i:
-     *          memo[i] = memo[2 * id - i] , simple mirror question
-     *      2: memo[2 * id - i] > mx - i:
-     *          memo[i] = mx - i, because we have the center id ,
-     *          and the left most of the longest palindrome centered at the mirror of i is lefter than the mirror of mx,
-     *          we assume that @1 the longest palindrome which centered at i can be longer,
-     *          but the mx is the right most of the longest palindrome which centered at id,
-     *          since @1 can be longer then the mx can be righter. contradiction! which means rightmost of the longest palindrome
-     *          which centered at i can't be righter than mx.
-     *       3: memo[2 * id - i] = mx - i:
-     *          actually, in this case
+     * ------the memo can help us! there are there inner cases here:
+     * ------1: memo[2 * id - i] < mx - i:
+     * ----------memo[i] = memo[2 * id - i] , doesn't need further computation
+     * ----------simple mirror question
+     * ------2: memo[2 * id - i] > mx - i:
+     * ----------memo[i] = mx - i, doesn't need further computation
+     * ----------because we have the center id ,
+     * ----------and the left most of the longest palindrome centered at the mirror of i is lefter than the mirror of mx,
+     * ----------we assume that @1 the longest palindrome which centered at i can be longer,
+     * ----------but the mx is the right most of the longest palindrome which centered at id,
+     * ----------since @1 can be longer then the mx can be righter. contradiction! which means rightmost of the longest palindrome
+     * ----------which centered at i can't be righter than mx.
+     * ------3: memo[2 * id - i] = mx - i:
+     * ----------actually, in this case we can say that memo[i] at least equals mx - i. And we need further computation
+     * ----------Because, the outer left of the leftmost of the longest palindrome( centered at id) must be different from the outer right of the rightmost of the longest palindrome( centered at id)
+     * ----------but the outer right can be the same as the outer left of the longest palindrome ( centered at i)
+     * After we have the memo[i], we try to update id and mx to a bigger value. This is because we can benefit from the longer palindrome( save computations)
+     * And we update the return relative values( maxI and longestLength)
      * @Params: [s]
      * @Create: 2019/12/2 17:03
      * @Return: java.lang.String
@@ -238,12 +152,108 @@ class Solution {
         }
         String newString = createFormattedString(s);
         int newLength = newString.length();
-        int[] p = new int[newLength];
+        int[] memo = new int[newLength];
         int mx = 0;
         int id = 0;
         int maxI = 0;
         int longestLength = 1;
         boolean needCompute;
-        return newString;
+        for (int i = 0; i < newLength; i++) {
+            if (i < mx) {
+                if (memo[2 * id - i] < mx - i) {
+                    memo[i] = memo[2 * id - i];
+                    needCompute = false;
+                } else if (memo[2 * id - i] > mx - i) {
+                    memo[i] = mx - i;
+                    needCompute = false;
+                } else {
+                    memo[i] = mx - i;
+                    needCompute = true;
+                }
+            } else {
+                memo[i] = 1;
+                needCompute = true;
+            }
+            if (needCompute) {
+                while (memo[i] + i < newLength && i - memo[i] >= 0 && newString.charAt(i + memo[i]) == newString.charAt(i - memo[i])) {
+                    memo[i]++;
+                }
+            }
+            if (i + memo[i] > mx) {
+                id = i;
+                mx = i + memo[i];
+                //debug
+                System.out.println("updating mx to " + mx + " and id to " + id);
+            }
+            if (memo[i] > longestLength) {
+                maxI = i;
+                longestLength = memo[i];
+            }
+            //debug
+            System.out.println("-----------");
+            for (int j : memo) {
+                System.out.print(j);
+                System.out.print(' ');
+            }
+            System.out.println("-----------");
+            for (int k = 0; k < newLength; k++) {
+                System.out.print(newString.charAt(k));
+                System.out.print(' ');
+            }
+            System.out.println("-----------");
+            for (int k = 0; k < newLength; k++) {
+                System.out.print(k);
+                System.out.print(' ');
+            }
+            System.out.println("-----------");
+        }
+        return newString.substring(maxI - longestLength + 1, maxI + longestLength).replace("#", "");
     }
+
+    /**
+     * @Description: expand
+     * helper function for center spread approach of the longest palindrome question
+     * it accepts the string and the left start index and right start index
+     * return the length of the longest palindrome with the start indexes.
+     * @Params: [s, left, right]
+     * @Create: 2019/12/2 22:38
+     * @Return: int
+     */
+    private int expand(String s, int left, int right) {
+        int left_index = left;
+        int right_index = right;
+        // stop condition : the sub string is not palindrome( s.left_index != s.right_index)
+        while (left_index >= 0 && right_index < s.length() && s.charAt(left_index) == s.charAt(right_index)) {
+            left_index--;
+            right_index++;
+        }
+        return right_index - left_index - 1;
+    }
+
+    /**
+     * @Description: longestPalindromeCenterSpread
+     * single digit expand and double digits expand
+     * then find the max and compare with before
+     * update start and end index
+     * @Params: [s]
+     * @Create: 2019/12/2 22:53
+     * @Return: java.lang.String
+     */
+    public String longestPalindromeCenterSpread(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int oddLength = expand(s, i, i);
+            int evenLength = expand(s, i, i + 1);
+            int length = Math.max(oddLength, evenLength);
+            if (length > end - start) {
+                // no matter in odd case or even case, we can get the left shift number by using (length-1)/2 , and get right shift number by using length/2;
+                start = i - (length - 1) / 2;
+                end = i + length / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+
 }
