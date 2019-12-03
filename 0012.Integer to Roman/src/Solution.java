c1e7e949fb6b854a93307e3f272ec30988de235a
hmq201211 <879602728@qq.com>
2019-12-03 23:17:57 +0800

    20191202

--- 0012.Integer to Roman/src/Solution.java
+++ 0012.Integer to Roman/src/Solution.java
@@ -1,4 +1,3 @@
-import com.sun.xml.internal.bind.v2.model.core.ID;
 
 /**
  * @ClassName: Leetcode->Solution
---
+++ 0014.Longest Common Prefix/src/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * @ClassName: Leetcode->Solution
+ * @Description: Write a function to find the longest common prefix string amongst an array of strings.
+ * <p>
+ * If there is no common prefix, return an empty string "".
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ * <p>
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * Note:
+ * <p>
+ * All given inputs are in lowercase letters a-z.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: Mingqing Hou
+ * @Create: 2019-12-03 23:00
+ **/
+public class Solution {
+    /**
+     * @Description: longestCommonPrefix
+     * @Params: [strs]
+     * @Create: 2019/12/3 23:01
+     * @Return: java.lang.String
+     */
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        while (true) {
+            boolean flag = true;
+            for (String s : strs) {
+
+            }
+        }
+    }
+}
