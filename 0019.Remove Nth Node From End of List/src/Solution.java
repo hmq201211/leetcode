f50d033838eae85fae5b69bf236e89ce1a651845
hmq201211 <879602728@qq.com>
2019-12-05 10:19:23 +0800

    191204

--- 0019.Remove Nth Node From End of List/src/Solution.java
+++ 0019.Remove Nth Node From End of List/src/Solution.java
@@ -1,4 +1,25 @@
+import java.util.List;
 
+/**
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ * <p>
+ * Example:
+ * <p>
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * <p>
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ * <p>
+ * Given n will always be valid.
+ * <p>
+ * Follow up:
+ * <p>
+ * Could you do this in one pass?
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 class ListNode {
     int val;
     ListNode next;
@@ -9,32 +30,25 @@ class ListNode {
 }
 
 public class Solution {
-    private int count;
-
+    /**
+     * @Description: removeNthFromEnd
+     * we have two pointers to maintain the distance of n
+     * when the faster pointer goes to the end
+     * we have the slower pointer goes just ahead of the target need to remove
+     * @Params: [head, n]
+     * @Create: 2019/12/5 9:30
+     * @Return: ListNode
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            count = 0;
-            return null;
-        }
-        ListNode next = removeNthFromEnd(head.next, n);
-        count++;
-        if (count == n) {
-            return next;
-        }
-        head.next = next;
-        return head;
-    }
-
-    public ListNode removeNthFromEnd2(ListNode head, int n) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode fast = dummy;
+        ListNode first = dummy;
         ListNode slow = dummy;
         for (int i = 0; i < n; i++) {
-            fast = fast.next;
+            first = first.next;
         }
-        while (fast != null) {
-            fast = fast.next;
+        while (first.next != null) {
+            first = first.next;
             slow = slow.next;
         }
         slow.next = slow.next.next;
--- 0020.Valid Parentheses/src/Solution.java
+++ 0020.Valid Parentheses/src/Solution.java
@@ -1,22 +1,72 @@
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.Stack;
 
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ * <p>
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ * <p>
+ * Input: "{[]}"
+ * Output: true
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Solution {
+    /**
+     * @Description: isValid
+     * we have a hashmap to store the key value pair of the mapping
+     * we have a stack container to help us do the validation
+     * if have a key then we push it to the stack
+     * if we have a value then we look the top of the stack if that item's mapping value is the same as the current one
+     * we say that this single validation is correct, and we can continue
+     * if the stack is empty or the mapping is wrong, then we just return false.
+     * @Params: [s]
+     * @Create: 2019/12/5 10:06
+     * @Return: boolean
+     */
     public boolean isValid(String s) {
-        HashMap<Character, Character> hm = new HashMap<>();
-        hm.put('[', ']');
-        hm.put('{', '}');
-        hm.put('(', ')');
+        if (s == null) return false;
+        HashMap<Character, Character> hashMap = new HashMap<>();
+        hashMap.put('(', ')');
+        hashMap.put('{', '}');
+        hashMap.put('[', ']');
         Stack<Character> stack = new Stack<>();
-        for (int index = 0; index < s.length(); index++) {
-            if (hm.containsKey(s.charAt(index))) {
-                stack.push(s.charAt(index));
+        char temp;
+        for (int i = 0; i < s.length(); i++) {
+            temp = s.charAt(i);
+            if (hashMap.containsKey(temp)) {
+                stack.push(temp);
             }
-            if (hm.containsValue(s.charAt(index))) {
+            if (hashMap.containsValue(temp)) {
                 if (stack.isEmpty()) {
                     return false;
                 }
-                if (hm.get(stack.pop()) != s.charAt(index)) {
+                if (temp != hashMap.get(stack.pop())) {
                     return false;
                 }
             }
