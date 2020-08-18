c1d13716c63b56b735857313ec1b41a9b1386fdb
hmq201211 <879602728@qq.com>
2020-08-18 21:23:15 +0800

    format

--- 0225.Implement Stack using Queues/src/MyStack.java
+++ 0225.Implement Stack using Queues/src/MyStack.java
@@ -5,12 +5,16 @@ public class MyStack {
 
     private Queue<Integer> queue;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public MyStack() {
         queue = new LinkedList<>();
     }
 
-    /** Push element x onto stack. */
+    /**
+     * Push element x onto stack.
+     */
     public void push(int x) {
         queue.add(x);
         int length = queue.size();
@@ -20,18 +24,24 @@ public class MyStack {
         }
     }
 
-    /** Removes the element on top of the stack and returns that element. */
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
     public int pop() {
         return queue.remove();
 
     }
 
-    /** Get the top element. */
+    /**
+     * Get the top element.
+     */
     public int top() {
         return queue.peek();
     }
 
-    /** Returns whether the stack is empty. */
+    /**
+     * Returns whether the stack is empty.
+     */
     public boolean empty() {
         return queue.isEmpty();
     }
